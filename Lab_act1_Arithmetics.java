23ba0eb61002f3fef7e19d56c6b7ec936b52e7ac
Stephen Colao <stephenbanaag0303@gmail.com>
2025-02-19 09:59:48 +0800

    Added Hello World Class

--- Lab_act1_Arithmetics.java
+++ Lab_act1_Arithmetics.java
@@ -1,27 +1,24 @@
 package Prelim;
 
+import java.util.Scanner;
 
 public class Lab_act1_Arithmetics {
 
     public static void main(String[] args) {
-       
-        
-        
-        int firstNumber = 10;
-        int secondNumber = 4;
-        int thirdNumber = 6;
 
-        
-        int result1 = firstNumber * secondNumber + thirdNumber;
-        int result2 = (firstNumber - secondNumber) % thirdNumber;
-        int result3 = (firstNumber + secondNumber + thirdNumber) / 3;
-        int result4 = firstNumber * thirdNumber - (secondNumber * secondNumber);
+        int x, y, z;
 
-       
-        System.out.println(firstNumber + " * " + secondNumber + " + " + thirdNumber + " = " + result1);
-        System.out.println("(" + firstNumber + " - " + secondNumber + ") % " + thirdNumber + " = " + result2);
-        System.out.println("(" + firstNumber + " + " + secondNumber + " + " + thirdNumber + ") / 3 = " + result3);
-        System.out.println(firstNumber + " * " + thirdNumber + " - (" + secondNumber + " * " + secondNumber + ") = " + result4);
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter num1: ");
+        x = in.nextInt();
+        System.out.println("Enter num2: ");
+        y = in.nextInt();
+        System.out.println("Enter num3: ");
+        z = in.nextInt();
+
+        System.out.println(x + " x " + y + " + " + z + " = " + (x * y + z));
+        System.out.println("(" + x + " - " + y + ")" + " % " + z + " = " + ((x - y) % z));
+        System.out.println("(" + x + " + " + z + " + " + y + " / " + "3" + " = " + ((x + y + z) / 3));
+        System.out.println(x + " * " + z + " - " + "(" + y + " * " + y + ")" + " = " + (x * z - (y * y)));
     }
 }
-    
