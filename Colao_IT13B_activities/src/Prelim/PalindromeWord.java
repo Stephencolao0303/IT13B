55dbc507efa094be4a346a2276659367f81f1c3e
Stephencolao0303 <stephenbanaag0303@gmail.com>
2025-03-09 23:34:34 +0800

    Added Palindrome Word

---
+++ Colao_IT13B_activities/src/Prelim/PalindromeWord.java
@@ -0,0 +1,25 @@
+
+package Palindrome;
+
+    import java.util.Scanner;
+
+public class PalindromeWord {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine();
+        String reversedWord = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+
+        if (word.equalsIgnoreCase(reversedWord)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+        scanner.close();
+    }
+}
